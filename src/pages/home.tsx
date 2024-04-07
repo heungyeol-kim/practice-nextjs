a1e1523e520b6f9fa54633a697916faef59e9ffa
heungyeol-kim <heungyeol.kim@musinsa.com>
2024-04-07 21:50:34 +0900

    Task 2: /home 에서 /product로 이동할 수 있게 합니다.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={() => router.push("/product")}>go to product</button>
     </div>
   );
 }
